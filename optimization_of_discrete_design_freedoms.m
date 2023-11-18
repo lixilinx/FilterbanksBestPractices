50ae7c9524e939ca7f7870fd0de9bd6a84a64011
lixilinx <37510137+lixilinx@users.noreply.github.com>
2023-11-18 14:36:27 -0800

    rand init guess is better for h and g

--- optimization_of_discrete_design_freedoms.m
+++ optimization_of_discrete_design_freedoms.m
@@ -6,7 +6,7 @@ figure;
 %% first with SAME symmetry constraint
 subplot(1,2,1)
 for B = 3:5
-    Costs = zeros(Lh, 1); % kind of silly to consider tau+1>Lh; just flip the filters to get tau+1>Lh
+    Costs = zeros(Lh+Lg, 1); % kind of silly to consider tau+1>Lh; just flip one to get another
     for tau = 1 : length(Costs)
         try
             fb = FilterBankStruct( );
@@ -18,8 +18,7 @@ for B = 3:5
             best_cost = inf;
             best_fb = fb;
             for num_trial = 1 : 100
-                [h, g] = fbd_random_initial_guess(Lh, Lg, fb.B, fb.tau0);
-                fb.h = h;   fb.g = g;
+                fb.h = rand(Lh,1);   fb.g = rand(Lg,1); % rand seems better than fbd_random_initial_guess :(
                 [fb, cost, recon_err, iter] = FilterBankDesign(fb, eta, lambda, 100);
                 fprintf('Trial: %g; cost: %g; reconstruction error: %g; iterations %g\n', num_trial, cost, recon_err, iter)
                 if cost < best_cost
@@ -49,7 +48,7 @@ title('(a) SAME symmetry')
 %% then free designs
 subplot(1,2,2)
 for B = 3:5
-    Costs = zeros(Lh, 1);
+    Costs = zeros(Lh+Lg, 1);
     for tau = 1 : length(Costs)
         try
             fb = FilterBankStruct( );
@@ -60,8 +59,7 @@ for B = 3:5
             best_cost = inf;
             best_fb = fb;
             for num_trial = 1 : 100
-                [h, g] = fbd_random_initial_guess(Lh, Lg, fb.B, fb.tau0);
-                fb.h = h;   fb.g = g;
+                fb.h = rand(Lh,1);   fb.g = rand(Lg,1);
                 [fb, cost, recon_err, iter] = FilterBankDesign(fb, eta, lambda, 100);
                 fprintf('Trial: %g; cost: %g; reconstruction error: %g; iterations %g\n', num_trial, cost, recon_err, iter)
                 if cost < best_cost
